63c2f619e20891d5837548a28a4a8219fe00631a
sunyangyang <jssqsy@qq.com>
2022-10-26 21:58:52 +0800

    create

---
+++ huoladuo-es-listen/src/main/java/com/huoladuo/HuoladuoEsListenApplication.java
@@ -0,0 +1,13 @@
+package com.huoladuo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuoladuoEsListenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuoladuoEsListenApplication.class, args);
+    }
+
+}
---
+++ huoladuo-es-listen/src/test/java/com/huoladuo/HuoladuoEsListenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huoladuo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuoladuoEsListenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
